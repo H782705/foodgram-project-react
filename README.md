81dabde67d9229d06b156ad563f132e75ceb2052
Дмитрий Курбанов <H782705@yandex.kz>
2022-02-11 22:49:43 +0600

    deploy settings

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# praktikum_new_diplom
+[![Foodgram workflow](https://github.com/H782705/foodgram-project-react/actions/workflows/foodgram_workflow.yml/badge.svg)](https://github.com/H782705/foodgram-project-react/actions/workflows/foodgram_workflow.yml)
+
 Первая попыткаad
--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -47,8 +47,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         many=True,
         read_only=True,
     )
-    is_favorited = serializers.BooleanField()
-    is_in_shopping_cart = serializers.BooleanField()
+    is_favorited = serializers.BooleanField(read_only=True)
+    is_in_shopping_cart = serializers.BooleanField(read_only=True)
 
     class Meta:
         model = Recipe
